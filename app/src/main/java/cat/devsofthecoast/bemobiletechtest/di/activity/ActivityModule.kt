b0e06ebab83f35a08fdd5fb2f81a6d5c013e5179
Xavier Arbat <xavierarbatmarquez@gmail.com>
2021-03-06 22:31:46 +0100

    add basis for hilt di

---
+++ app/src/main/java/cat/devsofthecoast/bemobiletechtest/di/activity/ActivityModule.kt
@@ -0,0 +1,11 @@
+package cat.devsofthecoast.bemobiletechtest.di.activity
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+class ActivityModule {
+
+}
