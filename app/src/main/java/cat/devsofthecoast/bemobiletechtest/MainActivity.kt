87c583712a3286f899b6e0ac119a60b7815d2e43
Xavier Arbat <xavierarbatmarquez@gmail.com>
2021-03-06 12:27:28 +0100

    basic empty activity android project

---
+++ app/src/main/java/cat/devsofthecoast/bemobiletechtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package cat.devsofthecoast.bemobiletechtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
