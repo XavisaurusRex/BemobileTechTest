67987f09560c1d4e13264be8ebe21f54629d7a49
Xavier Arbat <xavierarbatmarquez@gmail.com>
2021-03-11 16:21:24 +0100

    Improve navigation with BaseFragment and BaseViewHolder

--- app/src/main/java/cat/devsofthecoast/bemobiletechtest/feature/transacionsdashboard/view/viewmodel/TransactionsDashboardViewModel.kt
+++ app/src/main/java/cat/devsofthecoast/bemobiletechtest/feature/transacionsdashboard/view/viewmodel/TransactionsDashboardViewModel.kt
@@ -7,8 +7,11 @@ import androidx.lifecycle.viewModelScope
 import cat.devsofthecoast.bemobiletechtest.common.data.remote.AsyncResult
 import cat.devsofthecoast.bemobiletechtest.common.domain.AppDispatchers
 import cat.devsofthecoast.bemobiletechtest.common.domain.MutableSourceLiveData
+import cat.devsofthecoast.bemobiletechtest.common.view.viewmodel.BaseViewModel
+import cat.devsofthecoast.bemobiletechtest.feature.transacionsdashboard.domain.model.TransactionDetails
 import cat.devsofthecoast.bemobiletechtest.feature.transacionsdashboard.domain.usecase.RequestTransactionsToEurUseCase
 import cat.devsofthecoast.bemobiletechtest.feature.transacionsdashboard.view.adapter.dw.TransactionDataWrapper
+import cat.devsofthecoast.bemobiletechtest.feature.transacionsdashboard.view.fragment.TransactionsDashboardFragmentDirections
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,7 +21,7 @@ class TransactionsDashboardViewModel @Inject constructor(
     private val requestTransactionsToEurUseCase: RequestTransactionsToEurUseCase,
     private val dispatchers: AppDispatchers,
     private val savedStateHandle: SavedStateHandle
-) : ViewModel() {
+) : BaseViewModel() {
 
     private val _apiTransactions =
         MutableSourceLiveData<AsyncResult<List<TransactionDataWrapper>>>()
@@ -29,4 +32,7 @@ class TransactionsDashboardViewModel @Inject constructor(
         _apiTransactions.changeSource(requestTransactionsToEurUseCase.execute())
     }
 
+    fun goToTransactionDetails(transactionDetails: TransactionDetails){
+        navigate(TransactionsDashboardFragmentDirections.actionFromDashboardToDetails(transactionDetails))
+    }
 }
